bdb350099318266770e5ec571ac02816cfbee5de
vaibhav gupta <gupta.vaibhav1911@gmail.com>
2019-04-25 09:03:10 +0530

    quora vaibhav 1st commit

--- Backend/routes/answer.js
+++ Backend/routes/answer.js
@@ -1,49 +1,75 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var passport = require('passport');
-var requireAuth = passport.authenticate('jwt', {session: false});
-const mongoose = require('mongoose');
-var Model = require('../../Kafka-Backend/Models/answer');
+var passport = require("passport");
+var requireAuth = passport.authenticate("jwt", { session: false });
+const mongoose = require("mongoose");
+var Model = require("../../Kafka-Backend/Models/answer");
 
-router.post('/', requireAuth, function(req,res){
+router.post("/", requireAuth, function(req, res) {
   console.log("Inside answer Post Request");
   //if (req.session.user) {
   console.log("Inside answer Post Request");
-  console.log("Req Body : ",req.body);
-  
-console.log(req.body.question);
-req.body.question =  "abc";
+  console.log("Req Body : ", req.body);
 
+  console.log(req.body.question);
+  req.body.question = "abc";
 
-    req.body.question = "abc";
-    req.body.questionOwner = "Lucky";
-    req.body.isAnnonymous = true;
-    req.body.topic = "abc";
-    req.body.question="5cbf8898e35ac3ef9251d64b";
+  req.body.question = "abc";
+  req.body.questionOwner = "Lucky";
+  req.body.isAnnonymous = true;
+  req.body.topic = "abc";
+  req.body.question = "5cbf8898e35ac3ef9251d64b";
 
-    var user = new Model({
-        _id: new mongoose.Types.ObjectId(),
-        answer: req.body.answer,
-        answerOwner: req.user.id,
-        question: req.body.question,
-        isAnnonymous: req.body.isAnnonymous,
-        answerDate: Date.now()
-    });  
+  var user = new Model({
+    _id: new mongoose.Types.ObjectId(),
+    answer: req.body.answer,
+    answerOwner: req.user.id,
+    question: req.body.question,
+    isAnnonymous: req.body.isAnnonymous,
+    answerDate: Date.now()
+  });
 
-    user.save().then((doc) => {
-
-        console.log("Answer saved successfully.", doc);
-        res.value = "Answer saved successfully.";
-        res.end(JSON.stringify(res.value));
-    
-    }, (err) => {
-
-        console.log("Unable to save Answer details.", err);
-        res.value = "Unable to save Answer details.";
-        res.end(JSON.stringify(res.value));
-
-    });
-
+  user.save().then(
+    doc => {
+      console.log("Answer saved successfully.", doc);
+      res.value = "Answer saved successfully.";
+      res.end(JSON.stringify(res.value));
+    },
+    err => {
+      console.log("Unable to save Answer details.", err);
+      res.value = "Unable to save Answer details.";
+      res.end(JSON.stringify(res.value));
+    }
+  );
 });
 
-  module.exports = router;
+router.get("/answers", (req, res) => {
+  const errors = {};
+
+  console.log("query ", req.query.test);
+  console.log("query bdy", req.body.test);
+
+  const answersFields = {};
+
+  if (req.query.question) answersFields.question = req.query.question;
+  if (req.user._id) answersFields._id = req.user._id;
+
+  answersFields.answer = "abc";
+  answersFields.isAnnonymous = "true";
+
+  console.log("fields ans", answersFields);
+  Model.find(answersFields)
+
+    .then(answers => {
+      if (!answers) {
+        errors.noprofile = "There is no answers present for this search";
+        res.status(404).json(errors);
+      }
+
+      console.log("answers", answers.length);
+      res.json(answers);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+module.exports = router;
--- Kafka-Backend/Models/question.js
+++ Kafka-Backend/Models/question.js
@@ -1,22 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-mongoose.connect('mongodb+srv://canvas:canvas@cluster0-llr4m.mongodb.net/Canvas?retryWrites=true');
+var question = new Schema({
+  _id: mongoose.Schema.Types.ObjectId,
 
-var question = new Schema ({
+  question: { type: String },
+  questionOwner: { type: String },
+  topic: { type: String },
 
-    _id: mongoose.Schema.Types.ObjectId,
+  followers: [{ user: { type: Schema.Types.ObjectId, ref: "userDetails" } }],
 
-    question:{type:String},
-    questionOwner:{type:String},
-    topic:{type:String},
-    
-    followers: [{ user: { type: Schema.Types.ObjectId, ref: "userDetails" } }],
+  answer: [{ answer: { type: Schema.Types.ObjectId, ref: "answer" } }],
 
-    answer: [{ answer: { type: Schema.Types.ObjectId, ref: "answer" } }],
+  postDate: { type: Date }
+});
 
-    postDate:{type:Date},
-        
-    });
-
-    module.exports = mongoose.model("question",question);
+module.exports = mongoose.model("question", question);
