0cb34870a389bfd7bc6bad91b333258a847b38d4
Rohan shah <rohankumar.shah@sjsu.edu>
2019-05-01 03:50:03 -0700

    structural changes for question

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -10,10 +10,9 @@ import Home from "../Home/Home";
 import Answer from "../Answer/Answer";
 import "../../App.css";
 import "./Dashboard.css";
-import Questions from "../Questions/questions";
-import { getQuestions } from "../../Actions/questionsAction";
-import PropTypes from "prop-types";
-
+// import Questions from "../Questions/questions";
+// import { getQuestions } from "../../Actions/questionsAction";
+// import PropTypes from "prop-types";
 
 class Dashboard extends Component {
   constructor(props) {
@@ -22,56 +21,38 @@ class Dashboard extends Component {
     this.state = {};
   }
 
-  componentDidMount() {
-    this.props.getQuestions();
-  }
-
+  // componentDidMount() {
+  //   this.props.getQuestions();
+  // }
 
   //Dummy componenet did mount
   render() {
-    const { questions } = this.props.questions;
+    // const { questions } = this.props.questions;
 
-    if (questions === null)
-      return <div></div>
+    // if (questions === null)
+    //   return <div></div>
 
-
-    const questionsList = questions.map(question => (
-      <Questions question={question} />
-    ));
+    // const questionsList = questions.map(question => (
+    //   <Questions question={question} />
+    // ));
 
     return (
       <div>
         <Navbar />
 
         <div className="page-content">
-          {questionsList}
-          {/* 
-          <Switch>
-            <Route path="/home" component={Home} />
-            <Route path="/Answer" component={Answer} />
-            <Route path="/profile" component={Profile} />
-          </Switch> */}
+          {/*questionsList*/}
+          {
+            <Switch>
+              <Route path="/home" component={Home} />
+              <Route path="/Answer" component={Answer} />
+              <Route path="/profile" component={Profile} />
+            </Switch>
+          }
         </div>
       </div>
     );
   }
 }
 
-
-
-
-Dashboard.propTypes = {
-  getQuestions: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
-  questions: PropTypes.object.isRequired
-};
-
-const mapStateToProps = state => ({
-  loginStateStore: state.auth,
-  questions: state.questions
-});
-
-export default connect(
-  mapStateToProps,
-  { getQuestions }
-)(withRouter(Dashboard));
+export default withRouter(Dashboard);
