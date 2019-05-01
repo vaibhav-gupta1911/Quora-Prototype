31a7511223cb8a231dccc1f084a85c2f4eff5ab4
Tosha Kamath <toshakamath@gmail.com>
2019-04-30 21:18:25 -0700

    tosha meghna rohan vaibhav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,10 +8,6 @@ import Interests from "./components/Interests/Interests";
 import Login from "./components/Login/Login";
 import Dashboard from "./components/Dashboard/Dashboard";
 import "./App.css";
-import ConversationsList from "./components/Message/ConversationsList";
-import CreateMessage from "./components/Message/CreateMessage";
-import Inbox from "./components/Message/Inbox";
-import Photo from "./components/Message/ConversationsList";
 import 'react-router-modal/css/react-router-modal.css';
 import { ModalContainer, ModalRoute, Modal } from 'react-router-modal';
 
@@ -22,7 +18,7 @@ class App extends Component {
         <Router>
           <div className="App">
             <Switch>
-              <Route exact path="/" component={Dashboard} />
+              <Route path="/" component={Dashboard} />
               <Route
                 exact
                 path="/login"
@@ -37,12 +33,7 @@ class App extends Component {
                 path="/interests"
                 render={props => <Interests {...props} />}
               />
-              <Route path="/home/inbox/createmessage" component={CreateMessage}/>
-            {/* <Route path="/photo" component={Photo}/> */}
-            {/* <Route path="/home/inbox/displaymessages" component={DisplayMessage}/> */}
-            {/* <Route path="/home/inbox/:_id" component={Inbox}/> */}
-            {/* <ModalRoute path='/home/inbox/:_id' component={Inbox} /> */}
-            <Route path="/home/inbox" component={ConversationsList}/>
+              
             </Switch>
           </div>
         </Router>
--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -10,6 +10,8 @@ import Home from "../Home/Home";
 import Answer from "../Answer/Answer";
 import "../../App.css";
 import "./Dashboard.css";
+import ConversationsList from "../Message/ConversationsList";
+import CreateMessage from "../Message/CreateMessage";
 // import Questions from "../Questions/questions";
 // import { getQuestions } from "../../Actions/questionsAction";
 // import PropTypes from "prop-types";
@@ -44,6 +46,8 @@ class Dashboard extends Component {
           {/*questionsList*/}
           {
             <Switch>
+              <Route path="/home/inbox/createmessage" component={CreateMessage}/>
+            <Route path="/home/inbox" component={ConversationsList}/>
               <Route path="/home" component={Home} />
               <Route path="/Answer" component={Answer} />
               <Route path="/profile" component={Profile} />
